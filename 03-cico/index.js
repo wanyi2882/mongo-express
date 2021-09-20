570044d209b85f66802abbce78d6a0dd9670ebc4
wanyi2882 <wanyi.2882@gmail.com>
2021-09-20 08:06:42 +0000

    mongo 03-cico- edit and delete

--- 03-cico/index.js
+++ 03-cico/index.js
@@ -1,38 +1,50 @@
-const express = require("express")
-const hbs = require("hbs")
-const wax = require("wax-on")
-const MongoUtil = require("./MongoUtil")
+const express = require('express')
+const hbs = require('hbs');
+const { Db } = require('mongodb');
+const wax = require('wax-on')
+const MongoUtil = require('./MongoUtil')
+// import in ObjectId function from the mongodb package
+const ObjectId = require('mongodb').ObjectId;
 
-//setup dotenv library
-// const dotenv = require("dotenv")
+// setup dotenv library
+// const dotenv = require('dotenv');
 // dotenv.config();
-//or
-require("dotenv").config(); //nodejs will read the content of the .env file and put the variables inside there into the operating system
+// OR:
+require('dotenv').config(); // <-- nodejs will read the content of the .env
+                            // file and put the variables inside there
+                            // into the operating system
 
-//how to access variable in operating system
-//console.log(process.env.MONGO_URI)
+// how to access varaible in operating system?
+// console.log(process.env.MONGO_URI);
 
-let app = express()
-//use handlebars for the view engine
-app.set("view engine", "hbs")
-//set the public folder to store all the static files
-app.use(express.static("public"))
+let app = express();
+// use handlebars for the view engines
+app.set('view engine', 'hbs');
+// set the public folder to store all the static files
+app.use(express.static('public'));
 
-wax.on(hbs.handlebars)
-wax.setLayoutPath("./views/layouts")
+// enable template inheritance
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts');
 
-//enable forms
+// setup handlebars-heleprs
+const heleprs = require('handlebars-helpers')({
+    "handlebars": hbs.handlebars
+})
+
+// enable forms
 app.use(express.urlencoded({extended: false}));
 
 // ROUTES
 async function main(){
-    await MongoUtil.connect(process.env.MONGO_URI, "tgc14_cico")
+    await MongoUtil.connect(process.env.MONGO_URI, "tgc14_cico");
+
 
     app.get('/', async function(req,res){
-        res.send("welcome")
+       res.send("welcome")
     })
 
-    app.get("/food_record/add", function(req,res){
+    app.get('/food_record/add', function(req,res){
         res.render('add_food_record')
     })
 
@@ -80,11 +92,73 @@ async function main(){
             'records': records
         })
     })
+
+    // display the form that allows the user to edit the food record
+    app.get('/food_record/:food_record_id/edit', async function(req, res){
+        let db = MongoUtil.getDB();
+        // use findOne  when we expect only one result
+        // use find when we expect an array of results (have to remember to use toArray())
+        let foodRecord = await db.collection('food_records')
+                                            .findOne({
+                                                "_id":ObjectId(req.params.food_record_id)
+                                            })
+        res.render('edit_food_record',{
+            'foodRecord':foodRecord
+        })
+    })
+
+    app.post('/food_record/:food_record_id/edit', async function(req,res){
+        let db = MongoUtil.getDB();
+        let { foodRecordName, calories, tags} = req.body;
+         // normalize the tags 
+        // 1. if tags is undefined, normalize to empty array
+        tags = tags || [];
+        // 2. if tags is a string (i.e not an array), normalize to an array with that string as its only element
+        if (Array.isArray(tags) == false) {
+            tags = [ tags ];
+        }
+
+        await db.collection('food_records').updateOne({
+            '_id':ObjectId(req.params.food_record_id)
+        },{
+            '$set':{
+                // 'foodRecordName': foodRecordName,
+                // 'calories': calories,
+                // 'tags':tags
+                foodRecordName, calories, tags
+            }
+        })
+
+        res.redirect('/food_records')
+
+    })
+
+        // display the page that allows the user to delete the food record
+        app.get('/food_record/:food_record_id/delete', async function(req, res){
+            let db = MongoUtil.getDB();
+            // use findOne to find one record 
+            let foodRecord = await db.collection('food_records')
+                                                .findOne({
+                                                    "_id":ObjectId(req.params.food_record_id)
+                                                })
+            res.render('delete_food_record',{
+                'foodRecord':foodRecord
+            })
+        })
+
+        app.post('/food_record/:food_record_id/delete', async function(req,res){
+            let db = MongoUtil.getDB();
+            // use deleteOne when we expect only one result
+            await db.collection('food_records').deleteOne({
+                '_id':ObjectId(req.params.food_record_id)
+            })
+            res.redirect('/food_records')
+    
+        })
 }
 
-main()
+main();
 
 app.listen(3000, function(){
     console.log("Server has started")
 });
-
